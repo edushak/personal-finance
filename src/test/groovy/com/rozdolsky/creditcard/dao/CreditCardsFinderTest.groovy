b601d3c1af77f477f02e34767104f2b83459e6b7
edushak <edushak@yahoo.com>
2016-12-28 13:43:38 -0500

    Added a first test

--- src/test/groovy/com/rozdolsky/creditcard/dao/CreditCardsFinderTest.groovy
+++ src/test/groovy/com/rozdolsky/creditcard/dao/CreditCardsFinderTest.groovy
@@ -5,8 +5,7 @@ class CreditCardsFinderTest extends GroovyTestCase {
     CreditCardsFinder finder = new CreditCardsFinder()
 
     void testFindAll() {
-        def rows = finder.findAll()
-
+        List rows = finder.findAll()
         assert rows == [
             ['id': 1, 'name': 'Chase Rewards', 'number': '1234-1234-1234-1234', 'expirationMonth': 8, 'expirationYear': 2020, 'interestRate': 15, 'promoMonth': 3, 'promoYear': 2017, 'type': 2, 'vendorBank': 'Chase'],
             ['id': 2, 'name': 'Blue Rewards', 'number': '4444-4444-4444-4444', 'expirationMonth': 12, 'expirationYear': 2018, 'interestRate': 13.9, 'promoMonth': '', 'promoYear': '', 'type': 3, 'vendorBank': 'AmEx']
